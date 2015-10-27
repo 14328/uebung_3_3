363de6245b9387bfb5b6b3b3e93ec694ad625cd3
14328 <14328@A001-12.nordakademie.intern>
2015-10-27 13:21:11 +0100

    BMI-Calculator

---
+++ src/uebung_3_3/BMI.java
@@ -0,0 +1,154 @@
+package uebung_3_3;
+
+	public class BMI {
+		
+		public static void main(String[] args) {
+		
+		double gewicht = 62;
+		double groesse = 1.80;
+		double alter = 20;
+		boolean istweiblich = false;
+		
+		double bmi = gewicht / (groesse * groesse) ;
+		
+		
+		// Alter 64+
+		if (istweiblich = true && alter >= 64 && bmi <=29 && bmi >= 24){
+			
+			System.out.println("BMI ist im Normalbereich"); }
+			
+			else if (istweiblich = true && alter >= 64 && bmi >=30) {
+				System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+				
+			else if (istweiblich = true && alter >= 64 && bmi <=23)	{
+				System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!"); }
+		
+		
+		if (istweiblich = false && alter >= 64 && bmi <=30 && bmi >= 25){
+			
+			System.out.println("BMI ist im Normalbereich"); }
+			
+			else if (istweiblich = true && alter >= 64 && bmi >=31) {
+				System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+				
+			else if (istweiblich = true && alter >= 64 && bmi <=24)	{
+				System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!");}
+		
+		
+		// Alter 55-64
+		if (istweiblich = true && alter >= 55 && alter <= 63 && bmi <=28 && bmi >= 23){
+			
+			System.out.println("BMI ist im Normalbereich"); }
+			
+			else if (istweiblich = true && alter >= 55 && alter <= 63 && bmi >=29) {
+				System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+				
+			else if (istweiblich = true && alter >= 55 && alter <= 63 && bmi <=22)	{
+				System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!"); }
+		
+		
+		if (istweiblich = false && alter >= 55 && alter <= 63 && bmi <=29 && bmi >= 24){
+			
+			System.out.println("BMI ist im Normalbereich"); }
+			
+			else if (istweiblich = true && alter >= 55 && alter <= 63 && bmi >=30) {
+				System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+				
+			else if (istweiblich = true && alter >= 55 && alter <= 63 && bmi <=23)	{
+				System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!");}
+		
+		// Alter 45-54
+		if (istweiblich = true && alter >= 45 && alter <= 54 && bmi <=27 && bmi >= 22){
+			
+			System.out.println("BMI ist im Normalbereich"); }
+			
+			else if (istweiblich = true && alter >= 45 && alter <= 54 && bmi >=28) {
+				System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+				
+			else if (istweiblich = true && alter >= 45 && alter <= 54 && bmi <=21)	{
+				System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!"); }
+		
+		
+		if (istweiblich = false && alter >= 45 && alter <= 54 && bmi <=28 && bmi >= 23){
+			
+			System.out.println("BMI ist im Normalbereich"); }
+			
+			else if (istweiblich = true && alter >= 45 && alter <= 54 && bmi >=29) {
+				System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+				
+			else if (istweiblich = true && alter >= 45 && alter <= 54 && bmi <=22)	{
+				System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!");}
+		
+		// Alter 35-44 
+		if (istweiblich = true && alter >= 35 && alter <= 44 && bmi <=26 && bmi >= 21){
+			
+			System.out.println("BMI ist im Normalbereich"); }
+			
+			else if (istweiblich = true && alter >= 35 && alter <= 44 && bmi >=27) {
+				System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+				
+			else if (istweiblich = true && alter >= 35 && alter <= 44 && bmi <=20)	{
+				System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!"); }
+		
+		
+		if (istweiblich = false && alter >= 35 && alter <= 44 && bmi <=27 && bmi >= 22){
+			
+			System.out.println("BMI ist im Normalbereich"); }
+			
+			else if (istweiblich = true && alter >= 35 && alter <= 44 && bmi >=28) {
+				System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+				
+			else if (istweiblich = true && alter >= 35 && alter <= 44 && bmi <=21)	{
+				System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!");}
+		
+		// Alter 25-34 
+				if (istweiblich = true && alter >= 25 && alter <= 34 && bmi <=25 && bmi >= 20){
+					
+					System.out.println("BMI ist im Normalbereich"); }
+					
+					else if (istweiblich = true && alter >= 25 && alter <= 34 && bmi >=26) {
+						System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+						
+					else if (istweiblich = true && alter >= 25 && alter <= 34 && bmi <=19)	{
+						System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!"); }
+				
+				
+				if (istweiblich = false && alter >= 25 && alter <= 34 && bmi <=26 && bmi >= 21){
+					
+					System.out.println("BMI ist im Normalbereich"); }
+					
+					else if (istweiblich = true && alter >= 25 && alter <= 34 && bmi >=27) {
+						System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+						
+					else if (istweiblich = true && alter >= 25 && alter <= 34 && bmi <=20)	{
+						System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!");}
+				
+				
+		// Alter 24-
+			 if (istweiblich = true && alter <= 24 && bmi <=24 && bmi >= 19){
+					
+					System.out.println("BMI ist im Normalbereich"); }
+					
+					else if (istweiblich = true && alter <= 24 && bmi >=25) {
+						System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+						
+					else if (istweiblich = true && alter <= 24 && bmi <=18)	{
+						System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!"); }
+				
+				
+				if (istweiblich = false && alter <= 24 && bmi <=25 && bmi >= 20){
+					
+					System.out.println("BMI ist im Normalbereich"); }
+					
+					else if (istweiblich = true && alter <= 24 && bmi >=26) {
+						System.out.println("BMI ist nicht im Normalbereich - denk mal über Sport nach"); }
+						
+					else if (istweiblich = true && alter <= 24 && bmi <=19)	{
+						System.out.println("BMI ist nicht im Normalbereich - iss mehr!!!");}
+				
+			//Zusätzliche Ausgabe
+				
+			System.out.println("BMI = " + bmi);		
+			
+				}
+}
